3bd4fc15f71f43a1dc8288985455a7cc071792f5
Abdelmajid Sabiri <159253779+AbdelmajidSabiri@users.noreply.github.com>
2024-10-14 18:27:53 +0100

    Add input fields

--- client/src/pages/Add.jsx
+++ client/src/pages/Add.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+
 
 const Add = () => {
 
@@ -10,20 +13,56 @@ const Add = () => {
 
     });
 
+    const [error,setError] = useState(false)
 
-    const handlChange = (e) => {
-        setBooks(prev =>({...prev,[e.target.name] : e.target.value}));
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setBooks((prev) =>({...prev,[e.target.name] : e.target.value}));
     }
 
+    const handleClick = async (e) => {
+        e.preventDefault()
+        try {
+            await axios.post("http://localhost:8600/books",book);
+            navigate("/");
+        } catch (err) {
+            console.log(err);
+            setError(true);
+        }
+    }
 
     return(
         <div className="form">
-            <h1>Add new Book</h1>
-            <input type="text" placeholder="title" onChange={handlChange} name = "title"/>
-            <input type="text" placeholder="desc" onChange={handlChange} name = "desc"/>
-            <input type="text" placeholder="cover" onChange={handlChange} name = "cover"/>
-            <input type="text" placeholder="file" onChange={handlChange} name = "file"/>
-
+            <h1>Add New Book</h1>
+            <input
+                type="text"
+                placeholder="Book title"
+                name="title"
+                onChange={handleChange}
+            />
+            <textarea
+                rows={5}
+                type="text"
+                placeholder="Book desc"
+                name="desc"
+                onChange={handleChange}
+            />
+            <input
+                type="text"
+                placeholder="Book file"
+                name="Book file"
+                onChange={handleChange}
+            />
+            <input
+                type="text"
+                placeholder="Book cover"
+                name="cover"
+                onChange={handleChange}
+            />
+            <button onClick={handleClick}>Add</button>
+            {error && "Something went wrong!"}
+            <Link to="/">See all books</Link>
         </div>
     )
 }
