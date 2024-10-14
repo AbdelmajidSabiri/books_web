d486214ffa849cede3cfaa4a7f6de92f7b226f38
Abdelmajid Sabiri <159253779+AbdelmajidSabiri@users.noreply.github.com>
2024-10-14 17:31:41 +0100

    Use UseState and UseEffect hooks to fetch books data and display it

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -1,9 +1,48 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
 
 const Books = () => {
-    return(
-        <div>Books</div>
-    )
+
+    const [books,setBooks] = useState([])
+
+    useEffect(() => {
+        const fetchAllBooks = async() =>{
+            try {
+                const res = await axios.get("http://localhost:8600/books")
+                setBooks(res.data);
+                console.log(res)
+
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+        fetchAllBooks();
+    },[]);
+
+    
+    return (
+        <div>
+            <h1>Sabiri's Best Books</h1>
+            <div className="books">
+                {books.map (book => (
+                    <div className="book" key = {book.id}>
+                        {book.cover && <img src = {book.cover}/>}
+                        <h2>{book.title}</h2>
+                        <p>{book.desc}</p> 
+                        <span>{book.price}</span>
+                    </div>
+                ))}
+            </div>
+            <button><Link to="/add">Add new book</Link></button>
+        </div>
+    );
+
+    
 }
 
 
