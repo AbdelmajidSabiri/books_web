f07f447ed452ab091b5d65754aa6238530924d6e
Abdelmajid Sabiri <159253779+AbdelmajidSabiri@users.noreply.github.com>
2024-10-14 12:44:40 +0100

    Add get/post function

--- backend/index.js
+++ backend/index.js
@@ -10,11 +10,38 @@ const db = mysql.createConnection({
     database:"books_web"
 })
 
+app.use(express.json())
 
 app.get("/",(req,res) =>{
-    res.json("Hello how are youuuuuuuuuuuuu");
+    res.json("Hello");
 })
 
+app.get("/books", (req,res)=>{
+
+    const q = "SELECT * FROM books_web.books"
+
+    db.query(q,(err,data) =>{
+        
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+})
+
+app.post("/books",(req,res) =>{
+    const q = "INSERT INTO books (`title`,`desc`,`cover`,`book_path`) VALUES (?)"
+    const values = ["title from backend",
+                    "desc from backend",
+                    "cover pic from backend",
+                    "book_path from backend"]
+
+
+    db.query(q,[values],(err,data) => {
+
+        if(err) return res.json(err)
+            return res.json("Book has been created")
+
+    })
+})
 
 app.listen(8600, ()=>{
     console.log("Connected to backend")
