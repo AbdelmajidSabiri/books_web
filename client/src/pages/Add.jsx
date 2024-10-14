97e2c2503bd7aaf14263ab7f0e73ef1808d60986
Abdelmajid Sabiri <159253779+AbdelmajidSabiri@users.noreply.github.com>
2024-10-14 17:33:23 +0100

    Use UseState hook

--- client/src/pages/Add.jsx
+++ client/src/pages/Add.jsx
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Add = () => {
+
+    const [book,setBooks] = useState({
+        title : "",
+        desc : "",
+        cover : "",
+        book_path : "",
+
+    });
+
+
+    const handlChange = (e) => {
+        setBooks(prev =>({...prev,[e.target.name] : e.target.value}));
+    }
+
+
     return(
-        <div>Add</div>
+        <div className="form">
+            <h1>Add new Book</h1>
+            <input type="text" placeholder="title" onChange={handlChange} name = "title"/>
+            <input type="text" placeholder="desc" onChange={handlChange} name = "desc"/>
+            <input type="text" placeholder="cover" onChange={handlChange} name = "cover"/>
+            <input type="text" placeholder="file" onChange={handlChange} name = "file"/>
+
+        </div>
     )
 }
 
