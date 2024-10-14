b467efc6acb951d3e58f8efc1e262624886dbfb3
Abdelmajid Sabiri <159253779+AbdelmajidSabiri@users.noreply.github.com>
2024-10-14 17:34:50 +0100

    Add app.get and app.post methods of books page

--- backend/index.js
+++ backend/index.js
@@ -1,47 +1,56 @@
-import express from "express"
-import mysql from "mysql"
+import express from "express";
+import mysql from "mysql";
+import cors from "cors";
 
-const app = express()
+const app = express();
+app.use(express.json());
+app.use(cors());
+
 
 const db = mysql.createConnection({
     host:"localhost",
     user:"root",
     password:"Kgvo3539",
-    database:"books_web"
+    database:"books_web",
 })
 
-app.use(express.json())
 
 app.get("/",(req,res) =>{
     res.json("Hello");
 })
 
-app.get("/books", (req,res)=>{
+app.get("/books", (req, res) => {
+    const q = "SELECT * FROM books";
 
-    const q = "SELECT * FROM books_web.books"
+    db.query(q, (err, data) => {
+      if (err) {
+        console.log(err);
+        return res.json(err);
+      }
+      return res.json(data);
+    });
+});
 
-    db.query(q,(err,data) =>{
-        
-        if(err) return res.json(err)
-        return res.json(data)
-    })
-})
 
 app.post("/books",(req,res) =>{
     const q = "INSERT INTO books (`title`,`desc`,`cover`,`book_path`) VALUES (?)"
-    const values = ["title from backend",
-                    "desc from backend",
-                    "cover pic from backend",
-                    "book_path from backend"]
+    
+    const values = [
+        req.body.title,
+        req.body.desc,
+        req.body.price,
+        req.body.cover,
+        req.body.book_path,
+      ];
 
 
     db.query(q,[values],(err,data) => {
 
-        if(err) return res.json(err)
-            return res.json("Book has been created")
+        if(err) return res.json(err);
+        return res.json(data);
 
-    })
-})
+    });
+});
 
 app.listen(8600, ()=>{
     console.log("Connected to backend")
