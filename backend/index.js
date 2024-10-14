fde1c885873957950ee0b3652759b39216010d81
Abdelmajid Sabiri <159253779+AbdelmajidSabiri@users.noreply.github.com>
2024-10-14 22:19:07 +0100

    Add delete and put function to handl delete and update functions

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import cors from "cors";
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use('/images', express.static('public/images'));
 
 
 const db = mysql.createConnection({
@@ -14,10 +15,6 @@ const db = mysql.createConnection({
     database:"books_web",
 })
 
-
-app.get("/",(req,res) =>{
-    res.json("Hello");
-})
 
 app.get("/books", (req, res) => {
     const q = "SELECT * FROM books";
@@ -33,14 +30,13 @@ app.get("/books", (req, res) => {
 
 
 app.post("/books",(req,res) =>{
-    const q = "INSERT INTO books (`title`,`desc`,`cover`,`book_path`) VALUES (?)"
+    const q = "INSERT INTO books (`title`,`desc`,`cover`,`price`) VALUES (?)"
     
     const values = [
         req.body.title,
         req.body.desc,
-        req.body.price,
         req.body.cover,
-        req.body.book_path,
+        req.body.price,
       ];
 
 
@@ -51,6 +47,35 @@ app.post("/books",(req,res) =>{
 
     });
 });
+
+
+app.delete("/books/:id",(req,res) => {
+    const bookId = req.params.id
+    const q = "DELETE FROM books WHERE id = ?";
+
+    db.query(q,[bookId], (err,data) => {
+        if(err) return res.json(err);
+        return res.json(data);
+    });
+});
+
+app.put("/books/:id", (req, res) => {
+    const bookId = req.params.id;
+    const q = "UPDATE books SET `title` = ?, `desc` = ?, `cover` = ?, `price` = ? WHERE id = ?";
+  
+    const values = [
+      req.body.title,
+      req.body.desc,
+      req.body.cover,
+      req.body.price,
+    ];
+  
+    db.query(q, [...values,bookId], (err, data) => {
+      if (err) return res.json(err);
+      return res.json(data);
+    });
+  });
+  
 
 app.listen(8600, ()=>{
     console.log("Connected to backend")
